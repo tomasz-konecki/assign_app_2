a1a638583b89d7f674d5e5662345ebd7f4fd679e
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-02-08 13:28:29 +0100

    Add webpack

--- assign_app/routes/users.js
+++ assign_app/routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('user id = ');
 });
 
 module.exports = router;
